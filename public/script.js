0821177ce66699da1398cfcb508beccffc139728
Jason <kangja1996@gmail.com>
2021-04-05 13:01:40 -0400

    in progress

--- public/script.js
+++ public/script.js
@@ -1,21 +1,51 @@
+// const { PeerServer } = require("peer");
+
 const socket = io("/");
 const videoGrid = document.getElementById("video-grid");
 console.log(videoGrid);
 const myVideo = document.createElement("video");
 myVideo.muted = true;
 
+let peer = new Peer(undefined, {
+  path: "/peerjs",
+  host: "/",
+  port: "3030",
+});
+
 let myVideoStream;
 navigator.mediaDevices
   .getUserMedia({
     video: true,
-    audtio: true,
+    audio: false,
   })
   .then((stream) => {
     myVideoStream = stream;
     addVideoStream(myVideo, stream);
+
+    peer.on("call", (call) => {
+      call.answer(stream);
+      const video = document.createElement("video");
+      call.on("stream", (userVideoStream) => {
+        addVideoStream(video, userVideoStream);
+      });
+    });
+
+    socket.on("user-connected", (userId) => {
+      connectToNewUser(userId, stream);
+    });
   });
 
-socket.emit("joint-room");
+peer.on("open", (id) => {
+  socket.emit("join-room", ROOM_ID, id);
+});
+
+const connectToNewUser = (userId, stream) => {
+  const call = peer.call(userId, stream);
+  const video = document.createElement("video");
+  call.on("stream", (userVideoStream) => {
+    addVideoStream(video, userVideoStream);
+  });
+};
 
 const addVideoStream = (video, stream) => {
   video.srcObject = stream;
